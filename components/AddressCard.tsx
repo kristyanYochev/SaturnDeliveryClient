b3ce04663e73aa4af2547dedd5012d7f0a8cb11e
Kristyan Yochev <kris.yochev@gmail.com>
2021-02-27 10:14:06 +0200

    Refactored AddressCard

--- components/AddressCard.tsx
+++ components/AddressCard.tsx
@@ -1,79 +1,109 @@
 import { FunctionComponent, useState } from 'react';
-import { Button, Card } from 'semantic-ui-react';
-import { Header, Segment, Portal } from 'semantic-ui-react'
+import { Button, Card, Header, Portal, Segment } from 'semantic-ui-react';
 
 interface AddressCardProps {
-  fromAddress: String,
-  toAddress: String,
-  notes: String
+    fromAddress: string;
+    toAddress: string;
+    notes: string;
 }
 
-const AddressCard: FunctionComponent<AddressCardProps> = ({ fromAddress, toAddress, notes }) => {
-  const [open, setOpen] = useState(false);
+interface AddressCardPopupProps {
+    open: boolean;
+    onClose: () => void;
+    onAgree: () => void;
+    onDisagree: () => void;
+}
 
-  const closePopup = () => setOpen(false);
-  const openPopup = () => setOpen(true);
-  
-  return (
-    <>
-    <Card style={{
-          background: '#e5c7ff',
-        }}>
-        <Card.Content>
-            <Card.Header>Delivery order</Card.Header>
-            <Card.Description>
-                <strong>From: </strong> { fromAddress } <br/><hr />
-                <strong>To: </strong> { toAddress } <br/><hr />
-                <strong>Aditional Notes: </strong> <br/>
-                { notes }
-            </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-            <Button.Group fluid>
-                <Button style={{
-                    background: '#5fcf74',
-                  }}
-                  onClick={openPopup} 
-                  disabled={open}
-                  positive
-                >
-                    Approve
-                </Button>
-                <Button.Or />
-                <Button style={{
-                    background: '#d95959',
-                  }} color='red'>
-                    Decline
-                </Button>
-            </Button.Group>
-        </Card.Content>
-    </Card>
-    <Portal onClose={closePopup} open={open}>
+const AddressCardPopup: FunctionComponent<AddressCardPopupProps> = ({
+    open,
+    onClose,
+    onAgree,
+    onDisagree,
+}) => (
+    <Portal onClose={onClose} open={open}>
         <Segment
-          style={{
-            position: 'fixed',
-            top: '40%',
-            maxWidth: '300px',
-            left:0,
-            right:0,
-            margin: '0 auto',
-            zIndex: 1000,
-          }}
+            style={{
+                position: 'fixed',
+                top: '40%',
+                maxWidth: '300px',
+                left: 0,
+                right: 0,
+                margin: '0 auto',
+                zIndex: 1000,
+            }}
         >
             <Header>Are you sure you want to take this order?</Header>
-            <Button
-              content='Yes'
-              positive
-              onClick={closePopup}
-            />
-            <Button
-              content='No'
-              negative
-              onClick={closePopup}
-            />
+            <Button content='Yes' positive onClick={onAgree} />
+            <Button content='No' negative onClick={onDisagree} />
         </Segment>
-    </Portal></>
-  )
+    </Portal>
+);
+
+const AddressCard: FunctionComponent<AddressCardProps> = ({
+    fromAddress,
+    toAddress,
+    notes,
+}) => {
+    const [popupOpen, setPopupOpen] = useState(false);
+
+    const openPopup = () => setPopupOpen(true);
+    const closePopup = () => setPopupOpen(false);
+
+    const takeOrder = () => console.log('Order taken');
+
+    return (
+        <>
+            <Card
+                style={{
+                    background: '#e5c7ff',
+                }}
+            >
+                <Card.Content>
+                    <Card.Header>Delivery order</Card.Header>
+                    <Card.Description>
+                        <strong>From: </strong> {fromAddress} <br />
+                        <hr />
+                        <strong>To: </strong> {toAddress} <br />
+                        <hr />
+                        <strong>Aditional Notes: </strong> <br />
+                        {notes}
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button.Group fluid>
+                        <Button
+                            style={{
+                                background: '#5fcf74',
+                            }}
+                            onClick={openPopup}
+                            disabled={popupOpen}
+                            positive
+                        >
+                            Approve
+                        </Button>
+                        <Button.Or />
+                        <Button
+                            style={{
+                                background: '#d95959',
+                            }}
+                            color='red'
+                        >
+                            Decline
+                        </Button>
+                    </Button.Group>
+                </Card.Content>
+            </Card>
+            <AddressCardPopup
+                open={popupOpen}
+                onClose={closePopup}
+                onAgree={() => {
+                    closePopup();
+                    takeOrder();
+                }}
+                onDisagree={closePopup}
+            />
+        </>
+    );
 };
 
 export default AddressCard;
